8bb58e343c465e13c6733c99a5792741fa199d05
Stanislav Trubitsyn <trubitsynstanislav2004@gmail.com>
2026-03-04 16:37:07 +0100

    .

--- src/main/java/com/cryptotracker/cryptotracker/controller/MainController.java
+++ src/main/java/com/cryptotracker/cryptotracker/controller/MainController.java
@@ -1,10 +1,11 @@
 package com.cryptotracker.cryptotracker.controller;
 
+import com.cryptotracker.cryptotracker.entity.Portfolio;
 import com.cryptotracker.cryptotracker.service.CryptoService;
 import com.cryptotracker.cryptotracker.repository.PortfolioRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller 
 public class MainController {
@@ -19,10 +20,24 @@ public class MainController {
 
     @GetMapping("/") 
     public String index(Model model) {
-        
         model.addAttribute("btcPrice", cryptoService.getBtcPrice());
-        
         model.addAttribute("portfolios", portfolioRepository.findAll());
-        return "index.html"; 
+        return "index"; 
+    }
+
+    // Метод для створення портфеля
+    @PostMapping("/add")
+    public String addPortfolio(@RequestParam String name) {
+        Portfolio portfolio = new Portfolio();
+        portfolio.setName(name);
+        portfolioRepository.save(portfolio);
+        return "redirect:/"; // Перенаправляет на головну після збереження
+    }
+
+    // Метод для видалення портфеля
+    @GetMapping("/delete/{id}")
+    public String deletePortfolio(@PathVariable Long id) {
+        portfolioRepository.deleteById(id);
+        return "redirect:/";
     }
 }
